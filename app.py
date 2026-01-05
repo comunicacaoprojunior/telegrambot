86e94f625b62b4a6755e4ef148ef2528e02a4e77
comunicacaoprojunior <comunicacaoprojunior@gmail.com>
2026-01-05 03:08:55 -0300

    Update app.py

--- app.py
+++ app.py
@@ -41,7 +41,7 @@ async def caveirao(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("A PRO JUNIOR CHEGOUUU🔥🔥🔥 - DJ CARLOS!")
     await asyncio.sleep(1)
     await update.message.reply_animation(
-    animation="https://github.com/comunicacaoprojunior/telegrambot/blob/main/assets/Caveirao.gif?raw=true")
+    animation="https://raw.githubusercontent.com/comunicacaoprojunior/telegrambot/main/assets/Caveirao.gif")
 
 async def carute(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("carutê...🍷🥀")
@@ -82,6 +82,7 @@ def main():
 
 if __name__ == "__main__":
     main()
+
 
 
 
